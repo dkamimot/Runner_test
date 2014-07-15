3fc9f72c1cffebf34e248c6e6dbaf9261cf1cd11
dkamimot <dkamimot@gmail.com>
2014-07-15 13:21:38 -0400

    Game
    Unity Endless runner

---
+++ Assets/PowerUpScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpScript : MonoBehaviour {
+
+	HUDScript hud;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D other){
+	
+		if (other.tag == "Player") {
+			hud = GameObject.Find("Main Camera").GetComponent<HUDScript>();
+			hud.increaseScore(10);
+			Destroy (this.gameObject);
+		}
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+	public GameObject[] obj;
+	public Transform loca;
+	public int diff;
+	public float spawnMin = 1f;
+ 	public float spawnMax = 2f;
+
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+
+	void Spawn () {
+
+		Instantiate (obj [Random.Range (0, obj.GetLength (0))], new Vector3 (loca.position.x , loca.position.y + diff, 0), Quaternion.identity);
+		Invoke ("Spawn", Random.Range(spawnMin, spawnMax)); 
+
+	}
+
+}
